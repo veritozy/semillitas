4a841fe5e2f47ef814bea108a24e9dbf1babaa03
PabloOmega <pablonavarretectdb1c@gmail.com>
2025-12-03 12:33:42 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/organisms/AppBar.tsx

--- src/components/organisms/AppBar.tsx
+++ src/components/organisms/AppBar.tsx
@@ -1,17 +1,17 @@
 import {
     Flex,
-    // useTheme,
-    // Authenticator,
-    // useAuthenticator,
+    useTheme,
+    Authenticator,
+    useAuthenticator,
     Image,
-    // Icon,
+    Icon,
 } from '@aws-amplify/ui-react';
 import NavigationBar from '../molecules/NavigationBar';
-// import { signOut } from '@aws-amplify/auth';
-// import { useState } from 'react';
+import { signOut } from 'aws-amplify/auth';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-// import { BiSolidUserCircle as IconUser } from "react-icons/bi";
+import { BiSolidUserCircle as IconUser } from "react-icons/bi";
 
 const AppBar = ({ logo, pages }:
     {
@@ -19,24 +19,24 @@ const AppBar = ({ logo, pages }:
         pages: { title: string, path: string, submenu?: { title: string, path: string }[], hiddenOnAppBar?: boolean }[],
     }) => {
     const navigate = useNavigate();
-    // const { authStatus } = useAuthenticator(context => [context.authStatus]);
-    // const [showAuth, setShowAuth] = useState(false);
+    const { authStatus } = useAuthenticator(context => [context.authStatus]);
+    const [showAuth, setShowAuth] = useState(false);
     const isDesktop = useMediaQuery({ minWidth: 1224 });
     const isBigScreen = useMediaQuery({ minWidth: 1824 });
     const screenType = isBigScreen ? 'bigScreen' : isDesktop ? 'desktop' : 'mobile';
-    // const { tokens } = useTheme();
+    const { tokens } = useTheme();
 
-    // const handleSignInClick = () => {
-    //     setShowAuth(true);
-    // };
-    // async function handleSignOut() {
-    //     try {
-    //         await signOut();
-    //         setShowAuth(false);
-    //     } catch (error) {
-    //         console.log('error signing out: ', error);
-    //     }
-    // }
+    const handleSignInClick = () => {
+        setShowAuth(true);
+    };
+    async function handleSignOut() {
+        try {
+            await signOut();
+            setShowAuth(false);
+        } catch (error) {
+            console.log('error signing out: ', error);
+        }
+    }
 
     return (
         <Flex
@@ -52,22 +52,22 @@ const AppBar = ({ logo, pages }:
             <Flex alignItems="center" gap="large">
                 <Image height="40px" alt="Logo Semillitas" src={logo} onClick={() => navigate("/")} />
                 <NavigationBar pages={pages} screenType={screenType} />
-                {/* {
+                {
                     showAuth && (
-                        <Authenticator variation="modal" />
+                        <Authenticator variation="modal" socialProviders={['google']}/>
                     )
-                } */}
+                }
 
             </Flex>
             <Flex alignItems="center" gap="large">
-                {/* <Icon
+                <Icon
                     onClick={authStatus === 'authenticated' ? handleSignOut : handleSignInClick}
                     style={{ cursor: 'pointer' }}
                     as={IconUser}
                     ariaLabel="User Icon"
                     height="40px"
                     color={authStatus === 'authenticated' ? tokens.colors.green[50] : tokens.colors.blue[60]}
-                /> */}
+                />
             </Flex>
         </Flex>
     );
